76fcaff22490e95733985de355c3bd77b60393c8
cavalen <cavalen@hotmail.com>
2020-02-12 12:11:49 -0500

    Create install_ansible.sh

---
+++ install_ansible.sh
@@ -0,0 +1,8 @@
+# For Ubuntu 18.04 LTS
+# Install Ansible & Dependencies
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y docker.io docker-compose ansible
+pip3 install boto boto3 netaddr passlib f5-sdk bigsuds deepdiff 'ansible[azure]' 
+
+# Instalar Azure CLI 
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
