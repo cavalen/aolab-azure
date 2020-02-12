617e710363b2e107c835e60927cf78bc3ca0d00c
cavalen <cavalen@hotmail.com>
2020-02-12 12:30:47 -0500

    Update config_server.sh

--- script/config_server.sh
+++ script/config_server.sh
@@ -1,8 +1,9 @@
 !/bin/bash
 
-# Install Docker
+# Install Docker & Ansible
 sudo apt update
-sudo apt-get install -y docker docker-compose
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y software-properties-common ansible docker docker-compose
 echo -e "Default \e[34mDocker installed\e[0m"
 
 # Create lab containers
@@ -11,6 +12,7 @@ sudo docker run --name nginx --restart=unless-stopped -d -p 8081:80 nginx:latest
 sudo docker run --name dvwa --restart unless-stopped -d -p 8082:80 vulnerables/web-dvwa
 sudo docker run --name f5helloworld --restart unless-stopped -d -p 8083:8080 f5devcentral/f5-hello-world 
 sudo docker run --name juice-shop --restart=unless-stopped -d -p 8084:3000 bkimminich/juice-shop
+echo -e "Default \e[96mContainers created\e[0m"
 
 # Arcadia 
 sudo docker network create internal
@@ -20,4 +22,6 @@ sudo docker run -dit -h app2 --name=app2 --net=internal --restart unless-stopped
 sudo docker run -dit -h app3 --name=app3 --net=internal --restart unless-stopped registry.gitlab.com/mattdierick/arcadia-finance/app3:latest
 sudo docker run -dit -h nginx --name=nginx --net=internal --restart unless-stopped -p 80:80 -v /home/ubuntu/arcadia/default.conf:/etc/nginx/conf.d/default.conf registry.gitlab.com/mattdierick/arcadia-finance/nginx_oss:latest
 
-echo -e "Default \e[96mContainers created\e[0m"
+# Clone Lab Repos
+git clone https://github.com/wurstmeister/kafka-docker
+git clone https://github.com/cavalen/as3gosmarter/
