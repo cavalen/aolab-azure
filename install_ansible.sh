87d6d67d4bdf29bcb1c634628f7c4767d0092b0a
cavalen <cavalen@hotmail.com>
2020-03-31 22:27:32 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,7 @@
 # For Ubuntu 18.04 LTS
 # Install Ansible & Dependencies
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-add-repository --yes ppa:ansible/ansible
+sudo apt update && sudo apt -y upgrade
 sudo apt install -y docker.io python3-pip docker-compose ansible
 pip3 install boto boto3 netaddr passlib f5-sdk fi-cli bigsuds deepdiff 'ansible[azure]' 
 
