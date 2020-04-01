6c911a1c95830867e81f35e1a8a903b007a419b8
cavalen <cavalen@hotmail.com>
2020-03-31 22:54:01 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,7 +2,7 @@
 # Install Ansible & Dependencies
 sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt update && sudo apt -y upgrade
-sudo apt install -y docker.io python3-pip docker-compose ansible
+sudo apt install -y software-properties-common ansible docker.io docker-compose python3-pip
 pip3 install boto boto3 netaddr passlib f5-sdk fi-cli bigsuds deepdiff 'ansible[azure]' 
 
 # Install Azure CLI 
