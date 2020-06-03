22c052c7529c0effb5c7deeeccf61ecbf5961bdc
cavalen <cavalen@hotmail.com>
2020-06-03 15:09:04 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,7 +4,7 @@ sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt update
 sudo apt install -y software-properties-common ansible python3-pip
 #sudo apt install -y software-properties-common ansible docker.io docker-compose python3-pip
-pip3 install boto boto3 netaddr passlib f5-sdk f5-cli bigsuds deepdiff 'ansible[azure]' 
+pip3 install boto boto3 netaddr passlib bigsuds deepdiff 'ansible[azure]' 
 
 # Install Azure CLI 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
