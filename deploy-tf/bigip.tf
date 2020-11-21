d67473eaf5e087b07f60c3867d522b1d8347e641
cavalen <cavalen@hotmail.com>
2020-11-21 07:57:11 -0500

    testing ...

--- deploy-tf/bigip.tf
+++ deploy-tf/bigip.tf
@@ -221,7 +221,7 @@ resource "azurerm_virtual_machine_extension" "f5vm01-run-startup-cmd" {
         "fileUris": [
             "https://raw.githubusercontent.com/cavalen/aolab-azure/master/deploy-tf/bigip-runtime-init-install-only.yaml"
         ],
-        "commandToExecute": "mkdir -p /config/cloud; mkdir -p /var/log/cloud/azure; cp runtime-init-install-only.yaml /config/cloud/runtime-init-conf.yaml; curl -L https://cdn.f5.com/product/cloudsolutions/f5-bigip-runtime-init/v1.0.0/dist/f5-bigip-runtime-init-1.0.0-1.gz.run -o f5-bigip-runtime-init-1.0.0-1.gz.run && bash f5-bigip-runtime-init-1.0.0-1.gz.run -- '--cloud azure' 2>&1; f5-bigip-runtime-init --config-file /config/cloud/runtime-init-conf.yaml 2>&1"
+        "commandToExecute": "mkdir -p /config/cloud; mkdir -p /var/log/cloud/azure; cp bigip-runtime-init-install-only.yaml /config/cloud/runtime-init-conf.yaml; curl -L https://cdn.f5.com/product/cloudsolutions/f5-bigip-runtime-init/v1.0.0/dist/f5-bigip-runtime-init-1.0.0-1.gz.run -o f5-bigip-runtime-init-1.0.0-1.gz.run && bash f5-bigip-runtime-init-1.0.0-1.gz.run -- '--cloud azure' 2>&1; f5-bigip-runtime-init --config-file /config/cloud/runtime-init-conf.yaml 2>&1"
         }
     SETTINGS
 
