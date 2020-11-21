fe8d1de931713245581394eff5503fb4ee518fc0
cavalen <cavalen@hotmail.com>
2020-11-21 14:34:37 -0500

    Cambios ...

--- deploy-lab/deploy.sh
+++ deploy-lab/deploy.sh
@@ -1,9 +1,11 @@
-RED='\033[0;91m'
-REDBL='\033[5;91m'
-NC='\033[0m'
+#!/bin/bash
+#RED='\e[0;91m'
+#REDBL='\e[5;10m'
+#NC='\e[0m'
 echo "===================================================="
 echo "Cual es su Prefix ? (ej, su nombre, o sus iniciales): "
-echo "${RED}Usar maximo 10, solo letras\n${NC}"
+echo "Usar maximo 10, solo letras"
+echo ""
 read -p "Prefix: " prefix
 prefix=$(echo "$prefix" | tr -dc '[:alpha:]' | tr '[:upper:]' '[:lower:]' | head -c 10)
 
@@ -11,12 +13,16 @@ if [ -z "$prefix" ] || [ ${#prefix} -lt 2 ]
 then
    echo "Usando Prefix por defecto -student-"
    prefix="student"
-   sed -i 's/STUDENT_ID: cav/STUDENT_ID: student/g' config.yml
+   sed -i 's/.*STUDENT_ID:.*/STUDENT_ID: student/' config.yml
    echo ""
 else
    echo "Usando Prefix $prefix"
-   sed -i 's/STUDENT_ID: cav/STUDENT_ID: '$prefix'/g' config.yml
+   #sed -i 's/STUDENT_ID: cav/STUDENT_ID: '$prefix'/g' config.yml
+   sed -i 's/.*STUDENT_ID:.*/STUDENT_ID: '$prefix'/' config.yml
    echo ""
 fi
-echo "${REDBL}Desplegando .... \n\n${NC}"
-ansible-playbook 01_deploy_rg_vnet_azure.yml && ansible-playbook 02_deploy_ubuntu_docker_azure.yml && ansible-playbook 03_deploy_bigip_2nic_azure.yml && ansible-playbook 04_install_as3_ts_do.yml && ansible-playbook 05_get_information.yml &&
+echo ""
+echo "Desplegando .... "
+echo ""
+echo ""
+ansible-playbook 01_deploy_rg_vnet_azure.yml && ansible-playbook 02_deploy_ubuntu_docker_azure.yml && ansible-playbook 03_deploy_bigip_2nic_azure.yml && ansible-playbook 04_install_as3_ts_do.yml && ansible-playbook 05_get_information.yml
