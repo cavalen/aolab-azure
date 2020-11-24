1d10e9b86b670bada932110d0cd6fb4e85b8e6f7
cavalen <cavalen@hotmail.com>
2020-11-23 20:04:47 -0500

    Cambios ...

--- files/install-runtime-init.sh
+++ files/install-runtime-init.sh
@@ -1,5 +1,4 @@
 mkdir -p /config/cloud
-mkdir -p /var/log/cloud/azure
-curl https://raw.githubusercontent.com/cavalen/aolab-azure/master/deploy-tf/bigip-runtime-init-install-only.yaml -o /config/cloud/runtime-init-conf.yaml
+curl https://raw.githubusercontent.com/cavalen/aolab-azure/master/files/runtime-init-conf-install.yaml -o /config/cloud/runtime-init-conf.yaml
 curl -L https://cdn.f5.com/product/cloudsolutions/f5-bigip-runtime-init/v1.0.0/dist/f5-bigip-runtime-init-1.0.0-1.gz.run -o f5-bigip-runtime-init-1.0.0-1.gz.run && bash f5-bigip-runtime-init-1.0.0-1.gz.run -- '--cloud azure' 2>&1
 f5-bigip-runtime-init --config-file /config/cloud/runtime-init-conf.yaml 2>&1
